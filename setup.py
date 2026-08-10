f4b6b080be9aca5bca16cf9df43b38bc95dc3388
Pavel Kulyov <kulyov.pavel@gmail.com>
2026-08-10 17:04:56 +0300

    setup: drop deprecated test command definition

--- setup.py
+++ setup.py
@@ -3,18 +3,6 @@
 from pathlib import Path
 
 from setuptools import setup
-from setuptools.command.test import test as Test
-
-
-class PyTest(Test):
-    def finalize_options(self):
-        Test.finalize_options(self)
-        self.test_args = [ "tests" ]
-        self.test_suite = True
-
-    def run_tests(self):
-        import pytest
-        pytest.main(self.test_args)
 
 
 if __name__ == "__main__":
@@ -48,9 +36,5 @@ if __name__ == "__main__":
             "Programming Language :: Python :: Implementation :: CPython",
         ],
         platforms = [ "unix", "linux", "osx" ],
-
         packages = [ "python_config" ],
-
-        cmdclass = { "test": PyTest },
-        tests_require = [ "pytest" ],
     )
